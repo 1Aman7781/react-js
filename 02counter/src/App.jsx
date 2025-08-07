306873ea3f5c9c4eeccf628895b623287b533156
Aman <aman7781w@gmail.com>
2025-08-07 16:56:35 +0530

    learn about hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, amanCounter] = useState(10)
+
+
+  //let counter = 15
+
+  const addValue = () =>{
+    console.log("value added", counter);
+    counter = counter + 1
+    amanCounter(counter)
+  }
+
+  const removeValue = () =>{
+    amanCounter(counter - 1)
+    console.log("value added", counter);
+   
+  }
+
+  return (
+    <>
+     <h1>react</h1>
+     <h2>counter value: {counter}</h2>
+
+     <button
+     onClick={addValue}
+     >Add value</button>
+     <br />
+
+     <button
+     onClick={removeValue}
+     >remove value</button>
+    </>
+  )
+}
+
+export default App
